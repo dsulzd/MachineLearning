5b5e80277b2fbf1ca009ea861ccf4ce8fa99178d
dsulzd <bhavsardeep71@gmail.com>
2025-08-22 16:24:24 +0530

    Add new files

---
+++ LinearRegressionScratch/lr.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('data.csv')
+
+def loss_function(m, b, points):
+    total_error = 0
+    for i in range(len(points)):
+        x = points.iloc[i].Temperature
+        y = points.iloc[i].Revenue
+        total_error += (y - (m * x + b)) ** 2
+    return total_error / float(len(points))
+
+def gradient_descent(m_now, b_now, points, L):
+    m_gradient = 0
+    b_gradient = 0
+
+    n = len(points)
+
+    for i in range(n):
+        x = points.iloc[i].Temperature
+        y = points.iloc[i].Revenue
+
+        m_gradient += -(2/n) * x * (y - (m_now * x + b_now))
+        b_gradient += -(2/n) * (y - (m_now * x + b_now))
+    m = m_now - L * m_gradient
+    b = b_now - L * b_gradient
+
+    return m, b
+
+m = 0
+b = 0
+L = 0.0001
+epochs = 300
+
+for i in range(epochs):
+    if i % 50 == 0:
+        print(f"Epoch: {i}")
+    m, b = gradient_descent(m, b, data, L)
+
+print(m,b)
+
+plt.scatter(data.Temperature, data.Revenue, color='blue')
+plt.plot(list(range(0, 50)), [m * x + b for x in range(0, 50)], color='red')
+plt.show()
